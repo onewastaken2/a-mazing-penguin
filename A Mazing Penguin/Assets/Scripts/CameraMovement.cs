5e9bacad1377389c23a6741d4cff5212edd988ed
onewastaken2 <onewastaken2@gmail.com>
2021-12-29 21:15:48 -0800

    Collision detection added
    Player can be blocked by obstacles and die to enemies. Simple respawn added. Simple camera controls added. Cleaned up code.

---
+++ A Mazing Penguin/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float panSpeed = 10;   //How fast camera moves when panning
+
+
+    private void Update()
+    {
+        //Player pressed W to pan up
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * panSpeed * Time.deltaTime;
+        }
+
+        //Player pressed A to pan left
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * panSpeed * Time.deltaTime;
+        }
+
+        //Player pressed S to pan down
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * panSpeed * Time.deltaTime;
+        }
+
+        //Player pressed D to pan right
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * panSpeed * Time.deltaTime;
+        }
+    }
+}
--- A Mazing Penguin/Assets/Scripts/PlayerMovement.cs
+++ A Mazing Penguin/Assets/Scripts/PlayerMovement.cs
@@ -2,28 +2,41 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Camera mainCam;          //References game camera for raycasting mouse position
-    [SerializeField] private LayerMask groundLayer;   //For detecting when penguin is walking on ground terrain
-    [SerializeField] private LayerMask iceLayer;      //For detecting when penguin is sliding on ice terrain
-    [SerializeField] private Collider _collider;      //References collider for raycast origin for terrain detection
+    [SerializeField] private Camera mainCam;               //References game camera for raycasting mouse position
+    [SerializeField] private LayerMask groundLayer;        //For detecting when penguin is walking on ground terrain
+    [SerializeField] private LayerMask iceLayer;           //For detecting when penguin is sliding on ice terrain
+    [SerializeField] private LayerMask environmentLayer;   //For detecting walls and impassable objects
+    [SerializeField] private LayerMask hazardLayer;        //For detecting enemies and dangers
+    [SerializeField] private Collider _collider;           //References collider for raycast origin
+    [SerializeField] private GameObject _checkpoint;       //Referencing current level checkpoint
 
     private Vector3 clickPos;         //References where cursor is based on mouse click
-    private Quaternion clickPosRot;   //References mouse click position and rotates penguin to face it
+    private Vector3 slideTowards;     //Finds direction to slide to on mouse click without skates
+    private Vector3 checkpointPos;    //References checkpoint position for respawn location
+    private Quaternion clickPosRot;   //Rotates penguin towards mouse click based on current position with skates
 
-    private bool isMoving = false;    //For when player has clicked to move
-    private bool isSliding = false;   //For when penguin is moving over ice
+    public bool isMoving = false;     //For when player has clicked to move
+    public bool isSliding = false;    //For when penguin is moving over ice
+    public bool cannotStop = false;   //For when penguin is on ice without skates and CANNOT turn
+    public bool hasSkates = false;    //For when penguin is on ice with skates and CAN turn
 
     private float acceleration = 0;   //Allows for gradual increase in movement speed
     private float walkSpeed = 5;      //How fast penguin walks on ground
     private float slideSpeed = 8;     //How fast penguin slides on ice
-    private float turnSpeed = 6;      //How quickly penguin turns to face new given direction
+
+
+    private void Awake()
+    {
+        checkpointPos = _checkpoint.transform.position;
+    }
 
 
     private void Update()
     {
         RaycastHit hit;
-        
-        //Player is currently sliding on ice, and will check if penguin reaches ground terrain to stop movement
+
+        //Player is currently sliding on ice
+        //Checks if penguin reaches ground terrain to stop movement
         if(isSliding)
         {
             if(Physics.Raycast(_collider.bounds.center, transform.TransformDirection(Vector3.down), out hit, 1f, groundLayer))
@@ -32,12 +45,17 @@ public class PlayerMovement : MonoBehaviour
                 {
                     isSliding = false;
                     isMoving = false;
-                    acceleration = 0;
+
+                    if(cannotStop)
+                    {
+                        cannotStop = false;
+                    }
                 }
             }
         }
 
-        //Player is currently walking on ground terrain, and will check when penguin steps on ice to begin sliding
+        //Player is currently walking on ground terrain
+        //Checks when penguin steps on ice to begin sliding
         if(!isSliding)
         {
             if(Physics.Raycast(_collider.bounds.center, transform.TransformDirection(Vector3.down), out hit, 1f, iceLayer))
@@ -49,34 +67,61 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        //Player is currently moving the penguin
+        //Checks if the penguin is being blocked by something
+        if(isMoving)
+        {
+            if(Physics.SphereCast(_collider.bounds.center, 0.3f, transform.TransformDirection(Vector3.forward), out hit, 0.5f, environmentLayer))
+            {
+                if(hit.collider != null)
+                {
+                    isMoving = false;
+
+                    if(cannotStop)
+                    {
+                        cannotStop = false;
+                    }
+                }
+            }
+        }
+
         //Checks if player has pressed RMB, then penguin will move
-        if(Input.GetMouseButtonDown(1))
+        //Penguin will not turn and move in new direction if on ice without skates
+        if(Input.GetMouseButtonDown(1) && !cannotStop)
         {
             Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray, out hit, float.MaxValue, groundLayer | iceLayer))
             {
                 clickPos = hit.point;
                 clickPosRot = Quaternion.LookRotation(clickPos - transform.position);
+
+                if(!hasSkates || hasSkates && !isMoving)
+                {
+                    transform.rotation = clickPosRot;
+                    slideTowards = (clickPos - transform.position).normalized;
+                }
                 isMoving = true;
             }
         }
 
-        //Checks if player has pressed LMB while not moving, then penguin will turn to face new direction
+        //Checks if player has pressed LMB while not moving
+        //Penguin will turn to face new direction
         if(!isMoving)
         {
+            acceleration = 0;
+
             if(Input.GetMouseButtonDown(0))
             {
                 Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
                 if(Physics.Raycast(ray, out hit, float.MaxValue, groundLayer | iceLayer))
                 {
-                    clickPos = hit.point;
-                    clickPosRot = Quaternion.LookRotation(clickPos - transform.position);
+                    transform.rotation = Quaternion.LookRotation(hit.point - transform.position);
                 }
             }
-            LookTowards();
         }
 
-        //Checks if player has moved the penguin, then penguin will move based on terrain
+        //Checks if player has moved the penguin
+        //Penguin will move based on terrain
         if(isMoving && isSliding)
         {
             Slide();
@@ -88,10 +133,11 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    //Player has pressed RMB, and penguin will turn and move in the direction and position of mouse cursor
-    private void Move()
+    //Player has pressed RMB
+    //Penguin will turn and move in new direction and position of mouse cursor
+    void Move()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, clickPosRot, turnSpeed * Time.deltaTime);
+        transform.rotation = clickPosRot;
         transform.position = Vector3.MoveTowards(transform.position, clickPos, acceleration * Time.deltaTime);
 
         if(acceleration < walkSpeed)
@@ -102,27 +148,52 @@ public class PlayerMovement : MonoBehaviour
         if(transform.position == clickPos)
         {
             isMoving = false;
-            acceleration = 0;
         }
     }
 
 
-    //Penguin is now moving on ice, and will make gradual turns while RMB is pressed and move forward constantly
-    private void Slide()
+    //Penguin is now moving forward constantly on ice
+    //Makes gradual turns while RMB is pressed with skates
+    //Cannot turn at all without skates
+    void Slide()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, clickPosRot, turnSpeed * Time.deltaTime);
-        transform.position += transform.forward * acceleration * Time.deltaTime;
-
-        if(acceleration < slideSpeed)
+        if(hasSkates)
         {
-            acceleration += 0.25f;
+            transform.rotation = Quaternion.Slerp(transform.rotation, clickPosRot, 5.5f * Time.deltaTime);
+            transform.position += transform.forward * acceleration * Time.deltaTime;
+
+            if(acceleration < slideSpeed)
+            {
+                acceleration += 0.25f;
+            }
+        }
+        else
+        {
+            cannotStop = true;
+            transform.position += slideTowards * acceleration * Time.deltaTime;
+
+            if(acceleration < slideSpeed)
+            {
+                acceleration += 0.25f;
+            }
         }
     }
 
 
-    //Player has pressed LMB, and penguin will turn to face direction of mouse cursor
-    private void LookTowards()
+    //Detects for enemies and other dangers
+    private void OnTriggerEnter(Collider other)
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, clickPosRot, turnSpeed * Time.deltaTime);
+        if((hazardLayer & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+        {
+            Respawn();
+        }
+    }
+
+
+    //Player has died and is being sent back to checkpoint
+    void Respawn()
+    {
+        isMoving = false;
+        transform.position = checkpointPos;
     }
 }
